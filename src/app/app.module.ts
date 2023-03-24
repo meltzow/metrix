2554ee28b1ac57ee0534ee109cb08c8a2d68b06e
mario meltzow <dev@meltzow.net>
2023-03-24 23:06:38 +0100

    switching quantil line with slider is possible

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { NgChartsModule } from 'ng2-charts';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CycleTimeScatterplotComponent } from './cycle-time-scatterplot/cycle-time-scatterplot.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from "@angular/material/slider";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { CycleTimeScatterplotComponent } from './cycle-time-scatterplot/cycle-ti
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgChartsModule
+    NgChartsModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/cycle-time-scatterplot/cycle-time-scatterplot.component.ts
+++ src/app/cycle-time-scatterplot/cycle-time-scatterplot.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { BaseChartDirective } from "ng2-charts";
 
 @Component({
   selector: 'app-cycle-time-scatterplot',
@@ -6,8 +7,14 @@ import {Component} from '@angular/core';
   styleUrls: ['./cycle-time-scatterplot.component.sass']
 })
 export class CycleTimeScatterplotComponent {
+  // @ViewChild( BaseChartDirective ) chart: BaseChartDirective;
+
+  // private updateChart(){
+  //   this.chart.ngOnChanges({});
+  // }
+  quantil = 2.5;
   public linechart = {
-    "datasets": [
+    datasets: [
       {
         data: [
           {x: 1, y: 1},
@@ -19,7 +26,7 @@ export class CycleTimeScatterplotComponent {
         label: 'Series A',
         pointRadius: 10, type: 'scatter'
       },
-      {data: [2.5, 2.5, 2.5, 2.5, 2.5, 2.5], type: 'line'},
+      {data: [this.quantil, this.quantil, this.quantil, this.quantil, this.quantil, this.quantil], type: 'line', pointRadius: 0},
     ],
     labels: ["1","2","3","4","5","6"],
     "options": {
@@ -28,34 +35,10 @@ export class CycleTimeScatterplotComponent {
     }
   };
 
-  public chart = {
-    "datasets": [
-      {
-        data: [
-          {x: 1, y: 1},
-          {x: 2, y: 3},
-          {x: 3, y: -2},
-          {x: 4, y: 4},
-          {x: 5, y: -3},
-        ],
-        label: 'Series A',
-        pointRadius: 10, type: 'scatter'
-      },
-      {data: [2.5, 2.5, 2.5, 2.5, 2.5, 2.5], type: 'line', xAxisID: 'x2'}
-    ],
-    "labels": ["1", "2", "3", "4", "5"],
-    "options": {
-      scales: {
-        x: {
-
-        },
-        x2: { // add extra axes
-          position: 'bottom',
-          type: 'category',
-          display: true
-        },
-      }
-    }
-  };
+  valueChange(){
+    this.linechart.datasets[1]['data'] = [this.quantil, this.quantil, this.quantil, this.quantil, this.quantil, this.quantil];
+    // just trying refresh full variable
+    this.linechart.datasets = this.linechart.datasets.slice();
+  }
 
 }
